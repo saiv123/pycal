e79d37894bbe156431e724c9be53bbca09759867
saiv123 <saiveeravelli2000@gmail.com>
2016-10-24 17:13:02 -0700

    Add files via upload

---
+++ test3.py
@@ -0,0 +1,24 @@
+'''
+x = input("Type a number")
+if x == "5":
+        print("I am the best")
+else:
+        print(" worng")
+'''
+# start of the cal chalege
+
+num1 = int(input("first num"))
+num2 = int (input("second num"))
+op = input("the operation")
+if op == "+":
+        print(num1+num2)
+if op == "-":
+        print(num1-num2)
+if op == "*":
+        print(num1*num2)
+if op == "/":
+        print(num1/num2)
+if op == "^":
+        print(num1**num2)
+
+
